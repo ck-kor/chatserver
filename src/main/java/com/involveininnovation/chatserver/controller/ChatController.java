e55573ab99b5a650ce72b31751b3e5995fd87fea
ck-kor <ckdrb48@naver.com>
2022-07-09 20:12:44 +0900

    채팅스톰프

--- src/main/java/com/involveininnovation/chatserver/controller/ChatController.java
+++ src/main/java/com/involveininnovation/chatserver/controller/ChatController.java
@@ -1,29 +1,21 @@
 package com.involveininnovation.chatserver.controller;
 
-import com.involveininnovation.chatserver.model.Message;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.involveininnovation.chatserver.model.ChatMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
+@RequiredArgsConstructor
 @Controller
 public class ChatController {
 
-    @Autowired
-    private SimpMessagingTemplate simpMessagingTemplate;
+    private final SimpMessageSendingOperations messagingTemplate;
 
-    @MessageMapping("/message") // /app/message 이리로 보내면  (공개대화방 )
-    @SendTo("/chatroom/public") // 이리로 다 보내라?
-    public Message receiveMessage(@Payload Message message){
-        return message;
-    }
-
-    @MessageMapping("/private-message") // (개인 메시지 )
-    public Message recMessage(@Payload Message message){
-        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(),"/private",message); // /user/david/private
-        System.out.println(message.toString());
-        return message;
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 }
---
+++ src/main/java/com/involveininnovation/chatserver/model/ChatRoom.java
@@ -0,0 +1,20 @@
+package com.involveininnovation.chatserver.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class ChatRoom {
+    private String roomId;
+    private String name;
+    //채팅방 id + 생성한 사람의 이름
+    public static ChatRoom create(String name) {
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.roomId = UUID.randomUUID().toString();
+        chatRoom.name = name;
+        return chatRoom;
+    }
+}
